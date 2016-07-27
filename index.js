8c0d43b5a79f3c205e6a32a40565aca63390be56
Trey Shugart <treshugart@gmail.com>
2016-07-27 15:45:28 +1000

    refactor: Don't pass data from callbacks; simply use "this" that refers to the passed in "opts".
    It's up to the consumer to manage data on opts. This allows for defaults as well as state in the benchmarks.
    
    BREAKING CHANGE: Stop passing data args.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const browser = typeof window === 'object';
 
-function noop (next) {
-  next();
+function hrtime() {
+  return browser ? performance.now() : new Date().getTime();
 }
 
-function hrtime () {
-  return browser ? performance.now() : new Date().getTime();
+function next(next) {
+  next();
 }
 
 module.exports = function (func, opts = {}) {
   return new Promise(res => {
     let elapsed = 0
     let runs = 0;
-    
-    const after = opts.after || noop;
-    const before = opts.before || noop;
-    const time = opts.time || 1000;
+
+    opts.after = opts.after || next;
+    opts.before = opts.before || next;
+    opts.time = opts.time || 1000;
 
     function benchmark() {
-      if (elapsed < time) {
-        before(run.bind(null, after.bind(null, proceed)));
+      if (elapsed < opts.time) {
+        opts.before(run);
       } else {
-        res(runs / (time / 1000));
+        res(runs / (opts.time / 1000));
       }
     }
 
@@ -29,13 +29,13 @@ module.exports = function (func, opts = {}) {
       setTimeout(benchmark);
     }
 
-    function run(next, data) {
+    function run() {
       const now = hrtime();
-      func((...args) => {
+      func.call(opts, _ => {
         elapsed += hrtime() - now;
         ++runs;
-        next(...args);
-      }, data);
+        opts.after.call(opts, proceed);
+      });
     }
 
     benchmark();
--- test.js
+++ test.js
@@ -17,7 +17,9 @@ function work(cb, data) {
 
 describe('birdpoo', () => {
   it('should only run the function benchmark for the given time', done => {
-    bp(next => work(next), { time: 100 }).then(done.bind(null, null));
+    bp(next => work(next), { time: 100 })
+      .then(done.bind(null, null))
+      .catch(done);
   });
 
   it('should call the benchmark function', done => {
@@ -27,7 +29,8 @@ describe('birdpoo', () => {
       work(next);
     }, { time: 1 })
       .then(_ => assert.ok(called))
-      .then(done.bind(null, null));
+      .then(done.bind(null, null))
+      .catch(done);
   });
 
   it('should pass the operations per second to the promise', done => {
@@ -37,65 +40,40 @@ describe('birdpoo', () => {
         assert.equal(args.length, 1);
         assert.equal(typeof args[0], 'number');
       })
-      .then(done.bind(null, null));
+      .then(done.bind(null, null))
+      .catch(done);
   });
 
-  describe('before()', () => {
-    it('should be called', done => {
-      let called;
-      bp(next => work(next), {
-        before(next) {
-          called = true;
-          next();
-        },
-        time: 1
-      })
-        .then(() => assert.ok(called))
-        .then(done.bind(null, null));
-    });
-
-    it('should pass args to benchmark function', done => {
-      let obj = {};
-      let passed;
-      bp((next, data) => {
-        passed = data;
-        work(next);
-      }, {
-        before: next => next(obj),
-        time: 1
-      })
-        .then(() => assert.equal(passed, obj))
-        .then(done.bind(null, null));
-    });
+  it('before()', done => {
+    let called;
+    const opts = {
+      before(next) {
+        called = true;
+        assert.equal(this, opts);
+        next();
+      },
+      time: 1
+    };
+    bp(next => work(next), opts)
+      .then(() => assert.ok(called))
+      .then(done.bind(null, null))
+      .catch(done);
   });
 
-  describe('after()', () => {
-    it('should be called', done => {
-      let called;
-      bp(next => work(next), { 
-        after(next) {
-          called = true;
-          next();
-        },
-        time: 1
-      })
-        .then(() => assert.ok(called))
-        .then(done.bind(null, null));
-    });
-
-    it('should be passed args from the benchmark function', done => {
-      let obj = {};
-      let passed;
-      bp(next => work(next, obj), {
-        after: (next, data) => {
-          passed = data;
-          next();
-        },
-        time: 1
-      })
-        .then(() => assert.equal(passed, obj))
-        .then(done.bind(null, null));
-    });
+  it('after()', done => {
+    let called;
+    const opts = { 
+      after(next) {
+        called = true;
+        assert.equal(this, opts);
+        next();
+      },
+      time: 1
+    };
+    bp(next => work(next), opts)
+      .then(() => assert.ok(called))
+      .then(done.bind(null, null))
+      .catch(done);
   });
 
   it('async', done => {
@@ -103,6 +81,8 @@ describe('birdpoo', () => {
       after: next => setTimeout(next, 1),
       before: next => setTimeout(next, 1),
       time: 1
-    }).then(done.bind(null, null));
+    })
+      .then(done.bind(null, null))
+      .catch(done);
   });
 });
